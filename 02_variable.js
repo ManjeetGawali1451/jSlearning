ac7697660bd107b1db4900d70456d23919f54e74
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-20 11:56:31 +0530

    createdbasic.s

---
+++ 02_variable.js
@@ -0,0 +1,23 @@
+
+var myFullName;                        
+console.log(myFullName);
+var myFullName="Manjeet Gawali"
+console.log(myFullName);
+var myFullName="Manjeet Sanjay Gawali"
+console.log(myFullName);
+var myLove="I love Js"
+console.log(myLove);
+var firstName="First Name:Manjeet"
+console.log(firstName);
+var lastName="Last Name:Gawali"
+console.log(lastName);
+var age="age:22"
+console.log(age);
+var address="Address:Laxmi Road,Pune"
+console.log(address);
+var pincode="Pincode:411002"
+console.log(pincode);
+var gender="Gender:Male"
+console.log(gender);
+var isMarried="isMarried:No"
+console.log(isMarried);
---
+++ o1_variable.js
@@ -0,0 +1,19 @@
+
+var firstName; // variable declaration
+firstName = "Manjeet";  // Variable initialization
+console.log(firstName);
+firstName = "Mr. Manjeet"; // Variable update
+console.log(firstName);
+
+
+var lastName = "Gawali"; // variable declaration and initialization
+console.log(lastName);
+
+var height; // Variable Declaration
+console.log(height);
+height = "6"; // initialization
+console.log(height);
+height = "6.2 feet";// update
+console.log(height);
+height="5.9 feet";//update
+console.log(height);
