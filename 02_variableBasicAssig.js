7c84b9195695a376ceb15c4bc82e4730a1c7a0bd
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-21 19:16:04 +0530

    created02_variableBasicassign.js
    created02_variableBasicassign.js

---
+++ 02_variableBasicAssig.js
@@ -0,0 +1,39 @@
+
+
+console.log("-------Assignment 01------");
+console.log("-------(1st Part)-------");
+console.log("--------Number-----------");
+
+var pincode=413307
+console.log("Pin Code Number ==>",pincode,typeof(pincode));
+var mobileNumber=9970699800
+console.log("Mobile Number==>",mobileNumber,typeof(mobileNumber));
+var regNumber=567
+console.log("Reg Number==>",regNumber,typeof(regNumber));
+
+console.log("----------String---------");
+var firstName="Mohit"
+console.log("First Name==>",firstName,typeof(firstName));
+var lastName="Sharma"
+console.log("Last Name==>",lastName,typeof(lastName));
+var fullName="Mohit Rahul Sharma"
+console.log("Full Name==>",fullName,typeof(fullName));
+
+console.log("-------Boolean-------");
+var robot= false
+console.log("Are You Robot==>",robot,typeof(robot));
+var student=true
+console.log("Are You Student==>",student,typeof(student));
+var graduate=true
+console.log("Are You Graduate==>",graduate,typeof(graduate));
+
+
+console.log("---------Part 2--------");
+console.log("**********Diffrence Between undefined and Null");
+console.log("-----undefined");
+var myName=
+console.log("value==>",myName);
+
+console.log("----------Null Value--------");
+var nullvalue="null"
+console.log("value==>",nullvalue);
