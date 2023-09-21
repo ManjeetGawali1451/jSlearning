c9645c6e69f3013f952000369682d399d10d1b9a
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-21 19:34:20 +0530

    codeupdated
    codeupdated

--- 02_typeOf.js
+++ 02_typeOf.js
@@ -1,7 +1,7 @@
 
 
 console.log("-------Assignment 2------");
-console.log("-------Step1---------");
+console.log("****** Step1 ******");
 
 
 var bankName=
@@ -9,12 +9,12 @@ console.log("Bank Name==>",bankName);
 var bankName="SBI Bank"
 console.log("Bank Name==>",bankName,typeof(bankName));
 
-console.log("-------Step2--------");
+console.log("****** Step2 *******");
 
 var marks='90%'
 console.log("Marks==>",marks,typeof(marks));
 
-console.log("-------Step3--------");
+console.log("****** Step3 *******");
 
 var isWorking=
 console.log("Is Working==>",isWorking);
@@ -22,7 +22,7 @@ console.log("Is Working==>",isWorking);
 var isWorking=true
 console.log("Is Working==>",isWorking,typeof(isWorking));
 
-console.log("-------Step4-----");
+console.log("****** Step4 *******");
 console.log("-------Part a-----");
 var totalCount="Hundred and Seven"
 console.log("Total Count==>",totalCount,typeof(totalCount));
