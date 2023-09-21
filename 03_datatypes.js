c1fb13fd0e31ba335ce465151a25644aa7de4193
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-21 17:42:38 +0530

    created new file03_datatype.js
    created new file03_datatype.js

---
+++ 03_datatypes.js
@@ -0,0 +1,27 @@
+
+
+var firstName = "Manjeet";
+console.log(typeof firstName);
+var city = 'Pune';
+console.log("City=>" ,city, "data type==>" ,typeof city);
+
+var hscMarks= '78';
+console.log(hscMarks, typeof hscMarks);
+
+var isMarried = true;
+console.log(isMarried, typeof isMarried);
+
+var isHandicap = false;
+console.log(isHandicap, typeof(isHandicap));
+
+console.log(typeof "I love only JS");
+console.log(typeof 204567);
+console.log(typeof false);
+
+// Null and undefined
+
+console.log('--------null vs undefined-------');
+var accountNumber;
+console.log(accountNumber);
+accountNumber = null
+console.log(accountNumber);
