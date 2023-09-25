0a48b7c0f9d2c420e7f03cb2fc73652c97d0c165
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-25 19:47:58 +0530

    03_stringAssig01.js
    03_stringAssig01.js

---
+++ 03_stringBasicsAssig01.js
@@ -0,0 +1,14 @@
+
+
+function stringBasics(){ 
+            console.log("My dream company is Nvidia")
+            console.log("My Hobbies are:");
+            var myhobby1="Like to ride Bikes"
+            console.log("->",myhobby1);
+            var myhobby2="Coding"
+            console.log("->",myhobby2);
+            var myhobby3="Like to play Cricket"
+            console.log("->",myhobby3);
+            console.log("The sum of all hobbies are:43")
+           }
+            stringBasics("Like to ride Bikes","Coding","Like to play Cricket")
