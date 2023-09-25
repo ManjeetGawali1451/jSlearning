037e1adb2d26a15e6be4991520bfee740f1369f1
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-25 16:55:50 +0530

    03_funAssig02.js
    03_funAssig02.js

---
+++ 03_funAssig02.js
@@ -0,0 +1,15 @@
+
+
+console.log("======== Assignment 02 ============");
+console.log("========= Bank Details =========");
+function bankDetails(bankName,accountNum,location,pinCode){
+
+        console.log("Bank Name:",bankName);
+        console.log("Account Number:",accountNum);
+        console.log("City:",location);
+        console.log("Pincode:",pinCode);
+        console.log("----------------------------------------");
+}
+ bankDetails("CITI Bank",3456782345,'Pune',431202);
+ bankDetails("Axis Bank",7856782345,'Mumbai',441202);
+ bankDetails("HDFC Bank",895678245,'Pune',631202,"Open");
