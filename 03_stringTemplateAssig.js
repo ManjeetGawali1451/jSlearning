43d9458139f8d2f45e7d701195a0d2d7e522a5fb
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-26 17:15:10 +0530

    03_stringTemplateAssig.js
    03_stringTemplateAssig.js

---
+++ 03_stringTemplateAssig.js
@@ -0,0 +1,14 @@
+
+
+ console.log("======String Templete=======");
+
+           function stringBasics(){ 
+            console.log(`My dream company is "Nvidia"`)
+            var myhobby1="Coding"
+            var myhobby2="Like to Ride bikes"
+            var myhobby3="Like to Play cricket"   
+            console.log(`My hobbies are ==> ${myhobby1},  ${myhobby2},  ${myhobby3}`);
+            var result=myhobby1.concat(myhobby2,myhobby3)
+            console.log(`My hobbies after concatination==>${result}`);
+        }
+            stringBasics()
