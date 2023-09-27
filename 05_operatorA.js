2a395531d3c7e0a98150d161dd78ae8587b680a9
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-27 23:30:33 +0530

    codeupdated
    codeupdated

--- 05_operatorA.js
+++ 05_operatorA.js
@@ -1,7 +1,7 @@
 
 
 console.log("========= Assignment ========");
-
+console.log("=========== Step 1 ==========");
 function squareOfWordLength(str){
         var result=str.length
         return result;       
@@ -17,16 +17,36 @@ var result2=squareOfWordLength(10)
 var str2=squareOfWordLength("Google Chrome")
 var result3=squareOfWordLength(13)
 var str3=squareOfWordLength("Developer Smart")
-console.log("The length of given string is",str1);
-console.log("The  length square is",str1*str1);
-console.log("The length of given string 2 is :",str2);
-console.log("The length sqaure is:",str2*str2);
-console.log("The length of given string is",str3);
-console.log("The length sqaure is:",str3*str3);
+var result3=squareOfWordLength(15)
 
-// function square(num){
-                //var result = num * num; 
-                ///return result;
-//}
-//var squareNum = square(5);
-//console.log('Num is 5 and its square is: ', squareNum);
+console.log(`Given String:"Javascript"`);
+console.log(`The length of given string is ${str1}`);
+console.log(`The  length square is ${str1*str1}`);
+console.log("--------------------------------");
+console.log(`Given String:"Google Chrome"`);
+console.log(`The length of given string is ${str2}`);
+console.log(`The  length square is ${str2*str2}`);
+console.log("--------------------------------");
+console.log(`Given String:"Developer Smart"`);
+console.log(`The length of given string is ${str3}`);
+console.log(`The  length square is ${str3 *str3}`);
+console.log("--------------------------------");
+
+console.log("========== Step 2 =========");
+console.log("********** Function with no argument and no return type =========");
+console.log("--------- Section A -------");
+function reactDeveloper(){
+    var str="I am React Developer"
+    console.log("-------- Section A ----------");
+    var result=str.length
+    console.log('Given String is "I am React Developer"');
+    console.log(`The length of given string is ${result}`);
+    console.log("Total words available in the string is:4");
+    var result=20/4
+     console.log(`String length and divide by total number available in string is:${result}`);
+    var result=20*4
+
+    console.log("-------- Section B ----------");
+    console.log(`multiplication of given string is:${result}`);
+}
+reactDeveloper()
