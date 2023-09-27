db3cba173bd446e7865a20e2bc961de9c1d493a8
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-27 22:59:48 +0530

    lengthand its sqarefouned
    lengthand its sqarefouned

--- 05_operatorA.js
+++ 05_operatorA.js
@@ -3,9 +3,30 @@
 console.log("========= Assignment ========");
 
 function squareOfWordLength(str){
-
-    var lengthofStr=str.length
-     
+        var result=str.length
+        return result;       
+        
+        
+               
+        
     
+           
 }
-squareOfWordLength("Javascript")
+var str1=squareOfWordLength("Javacsript")
+var result2=squareOfWordLength(10)
+var str2=squareOfWordLength("Google Chrome")
+var result3=squareOfWordLength(13)
+var str3=squareOfWordLength("Developer Smart")
+console.log("The length of given string is",str1);
+console.log("The  length square is",str1*str1);
+console.log("The length of given string 2 is :",str2);
+console.log("The length sqaure is:",str2*str2);
+console.log("The length of given string is",str3);
+console.log("The length sqaure is:",str3*str3);
+
+// function square(num){
+                //var result = num * num; 
+                ///return result;
+//}
+//var squareNum = square(5);
+//console.log('Num is 5 and its square is: ', squareNum);
