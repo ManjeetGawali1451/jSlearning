7c38bd08d20fbb1c6d4d5e8b59216b04ae7c4e96
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-28 23:59:37 +0530

    05_opeartaorTernaryAssigniscreated
    05_opeartaorTernaryAssigniscreated

---
+++ 05_operatorTernaryAssigC.js
@@ -0,0 +1,17 @@
+
+
+console.log("******** Assignment *********");
+console.log("======== Male Marraige Eligibility =========");
+
+function maleMarriageEligibility(gender,age,boyName) {
+        var result=gender=='Male' && age>=21
+        ?`Hey ${boyName} You are Eligible for marriage`
+        :`Sorry, ${boyName} You are not eligible for marriage `
+        return result
+        
+    
+}
+var male = maleMarriageEligibility("Male",25,"Billgates")
+var male2 = maleMarriageEligibility("Male",17,"Stew Jobs")
+console.log(male);
+console.log(male2);
