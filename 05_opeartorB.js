775ca3ad0d966c56d763e0a0b1a1daf881a4d889
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-28 12:42:03 +0530

    step1 and step2complete
    step1 and step2complete

---
+++ 05_opeartorB.js
@@ -0,0 +1,34 @@
+
+
+console.log("---------Assigment B-----------");
+
+console.log("------------ Step 1 -----------");
+console.log("------------- greater Number -----------");
+function greaterNumber(num1,num2){
+      var result=num1>num2?"Num1 is greater than Num2":"False"
+       console.log(`${num1} is graeter than ${num2}`);
+             
+}        
+greaterNumber(10,-10)
+greaterNumber(899,800)
+
+console.log("------------ Step 2 ----------");
+
+function isEvenorOdd(values) {
+        var num1=29
+        var num2=44
+        var num3=0
+        var num4=101
+        var result=num1%2==0?"Even":"odd"
+        var result2=num2%2==0?"Even":"odd"
+        var result3=num3%2==0?"Even":"odd"
+        var result4=num4%2==0?"Even":"odd"
+        console.log(`${num1} is ${result}`);
+        console.log(`${num2} is ${result2}`);
+        console.log(`${num3} is ${result3}`);
+        console.log(`${num4} is ${result4}`);
+}
+isEvenorOdd()
+
+
+
