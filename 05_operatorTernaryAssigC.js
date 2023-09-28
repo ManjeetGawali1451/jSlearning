78c47c3a4f0244c97f77d5c8cd1f028a022e49d9
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-29 00:15:24 +0530

    step2completed
    step2completed

--- 05_operatorTernaryAssigC.js
+++ 05_operatorTernaryAssigC.js
@@ -15,3 +15,15 @@ var male = maleMarriageEligibility("Male",25,"Billgates")
 var male2 = maleMarriageEligibility("Male",17,"Stew Jobs")
 console.log(male);
 console.log(male2);
+
+console.log("=========== Step 2 ===========");
+function femaleMarraigeEligibility(gender,age,girlName) {
+    var result=gender=='Female'&& age<=18
+    ?`Sorry ${girlName} You are not eligible for marraige `
+    :`Hey ${girlName} You are eligible for marriage`
+    return result;
+}
+var female = femaleMarraigeEligibility("Female",16,"Jenifer")
+var female2 = femaleMarraigeEligibility("Female",27,"Malinda Gates");
+console.log(female);
+console.log(female2);
