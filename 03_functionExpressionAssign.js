bb15cb7d5797712aaf86fd3d9f1e96658f1b802b
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-09-29 16:49:44 +0530

    step1 completed
    step1 completed

---
+++ 03_functionExpressionAssign.js
@@ -0,0 +1,23 @@
+
+console.log("********* Assignment ***********");
+console.log("========== Function Expression =======");  
+console.log("---------- Step 1 ----------");
+
+
+var square=function(values){
+        return values*values
+}
+var square1=square(5)
+var square2=square(6)
+var square3=square(25)
+var square4=square(100)
+var square5=square(67)
+var square6=square(89)
+var square7=square(59)
+console.log(`The square of 5 is ${square1}`);
+console.log(`The square of 6 is ${square2}`);
+console.log(`The square of 25 is ${square3}`);
+console.log(`The square of 100 is ${square4}`);
+console.log(`The square of 67 is ${square5}`);
+console.log(`The square of 89 is ${square6}`);
+console.log(`The square of 59 is ${square7}`);
