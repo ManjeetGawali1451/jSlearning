9d0a998e7900052db66380285e1d76b70e14faf8
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-02 19:46:38 +0530

    step1 completed
    step1 completed

--- 06_for_Assignment01.js
+++ 06_for_Assignment01.js
@@ -2,3 +2,7 @@
 
 console.log("========= Assignment =======");
 console.log("********* For Loop ********");
+console.log("--------- Step 1 --------");
+for (let  index =5 ; index<=15; index++) {
+    console.log(index)
+}
