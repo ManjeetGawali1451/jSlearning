6df96e09c15d87af43efcdc60fd6aa84eea21a71
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-02 19:52:15 +0530

    step2completed
    step2completed

--- 06_for_Assignment01.js
+++ 06_for_Assignment01.js
@@ -3,6 +3,14 @@
 console.log("========= Assignment =======");
 console.log("********* For Loop ********");
 console.log("--------- Step 1 --------");
+console.log("======= 5 to 15 numbers =======");
 for (let  index =5 ; index<=15; index++) {
     console.log(index)
 }
+
+console.log("--------- Step 2 ------------");
+ for (let index = 50; index>=40; index--){
+        console.log(index);
+
+}
+    
