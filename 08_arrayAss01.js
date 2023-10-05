9e1b5252b4b16e16b1da8ae06491e51f53134829
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-05 22:52:45 +0530

    08_arrayAss01created
    08_arrayAss01created

---
+++ 08_arrayAss01.js
@@ -0,0 +1,16 @@
+
+console.log("----- Assignment ------");
+console.log("****** Step 1 ******");
+let array=[21,11,44,55,77,33]
+console.log("In given array the even elements are");
+for (let index = 0; index < array.length; index++) {
+    
+    if (index%2==0) {
+        
+        console.log(array[index]);
+        
+    }
+    
+}
+
+   
