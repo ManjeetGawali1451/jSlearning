e3d5c66bef0df8ceb40beae282592c84062c9f97
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-09 23:46:26 +0530

    taskcompleted
    taskcompleted

--- 09_objectCloneassign.js
+++ 09_objectCloneassign.js
@@ -1,2 +1,43 @@
 
 console.log("--------- Assignment --------");
+console.log("Bank Sbi literals:");
+
+ let bankSbi = {
+          Name:"Mahesh Mishra",
+          BankName:"State Bank Of India",
+          AccNo:556677788990,
+          Ifsccode:'566778899'
+ };
+ let bankLocation ={
+             street:"SV road",
+             city:"Mumbai",
+             pin:400003
+
+ };
+ let rateOfInterest={
+               homeLoanInterset:"7.5%",
+               personalLoanInterset:"8.2%",
+               duelInterest:"5.5%"
+
+ } 
+ console.table(bankSbi);
+ console.log("bank location literals");
+console.table(bankLocation);
+ console.log("Bank sbi object cloned to bank clone");
+ const bankclone = Object.assign({},bankSbi );
+ console.table(bankclone);
+ console.log("Bank Location object cloned with bank deatils");
+ const bankDetails=Object.assign({},bankLocation)
+ console.table(bankDetails);
+ console.log("Rate of Interest Literals:");
+ console.table(rateOfInterest);
+ console.log("Merged step1 , step2 and step4 with Sbi Details Object");
+const SbiDetails=Object.assign(bankSbi,bankLocation,rateOfInterest)
+console.table(SbiDetails);
+console.log("Transversing of  merged object");
+for (const key in SbiDetails) {
+                    console.log(  key     , SbiDetails[key]);                                                     
+ }
+
+
+ 
