11f92f803688cc47e0c2fa2a9f3e44387b4cfe03
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-11 21:58:47 +0530

    codeUpdated
    codeUpdated

--- 09_constructor_funAssign.js
+++ 09_constructor_funAssign.js
@@ -7,11 +7,17 @@ function Bank(bankName,location,ifscCode,branchCode){
       this.ifscCode=ifscCode;
       this.branchCode=branchCode;
 }
-const yesBank= new Bank("yesBank","Maharashtra", " YesBo1666 ", "Pune")
-const sbiBank = new Bank("sbiBank", "Karnataka", " SBIN0021733 ",   "Bangalore");
-const mahBank = new Bank("mahBank", "Mumbai",    " MSCI0082016 ",   "Borewali");
-const axisBank = new Bank("axisBank", "Assam",    " UTIB0001190 ",   "Guwahati");
+const yesBank= new Bank("yesBank","Maharashtra", " YesBo1666 ", " 415556 ")
+const sbiBank = new Bank("sbiBank", "Karnataka", " SBIN0021733 ", " 516667 ");
+const mahBank = new Bank("mahBank", "Mumbai",    " MSCI0082016 ",   " 618898 ");
+const axisBank = new Bank("axisBank", "Assam",    " UTIB0001190 ",   " 419923");
 console.log(`Bank Deatils: ${yesBank.bankName} ${yesBank.location} ${yesBank.ifscCode} ${yesBank.branchCode} `);
 console.log(`Bank Deatils: ${sbiBank.bankName} ${sbiBank.location} ${sbiBank.ifscCode} ${sbiBank.branchCode}`); 
 console.log(`Bank Deatils: ${mahBank.bankName} ${mahBank.location} ${mahBank.ifscCode} ${mahBank.branchCode}`);
 console.log(`Bank Deatils: ${axisBank.bankName} ${axisBank.location} ${axisBank.ifscCode} ${axisBank.branchCode}`);
+console.log("=======================================================================================");
+Bank.prototype.opentime="9 AM"
+Bank.prototype.closetime="6 PM"
+console.log(`The opening time of sbi bank is ${sbiBank.opentime} and the closing time of Sbi bank is ${sbiBank.closetime}`);
+console.log(`The bank name is ${axisBank.bankName} and the closing time  is ${axisBank.closetime}`);
+console.log(`The bank name is ${yesBank.bankName} the branch Code is ${axisBank.branchCode} and the closing time  is  ${axisBank.closetime}`);
