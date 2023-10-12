291bac1cf0e1a40cbb0274fae66000955e8a3a9b
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-12 20:22:12 +0530

    codeadded
    codeadded

--- 09_objectCloning.js
+++ 09_objectCloning.js
@@ -46,3 +46,39 @@ elonPerson.city= "Mumbai";
 console.log(elonPerson.city); // 
 
 console.log(person.city);
+
+let stdNagesh = {
+  name: "Nagesh",
+
+  age: 22,
+
+  graduation: "BE - Computer Science",
+};
+
+let stdGanesh = { ...stdNagesh }; // Deep clone using spread operator
+
+stdGanesh.name = "Ganesh";
+
+console.log(stdNagesh.name);
+
+let stdJenny = {
+  name: "Jenny",
+
+  age: 22,
+
+  gender: "Female",
+
+  marks: {
+    computer: 96,
+
+    math: 78,
+
+    english: 56,
+  },
+};
+
+let stdBill = JSON.parse(JSON.stringify(stdJenny)); // Deep clone when we have nested object
+
+stdBill.name = "Billgates";
+
+console.log(stdJenny.name);
