a627d5c4712f9eceb7811ebef4745707a0f7a52e
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-12 22:52:17 +0530

    step1completed
    step1completed

---
+++ 09_objectCloneAssig.js
@@ -0,0 +1,9 @@
+console.log("------ Assignment --------");
+
+let personDetails={
+         firstName:'Mohit',
+         lastName:'Mishra',
+         age:'22',
+         Education:'MBA'           
+}
+console.log(personDetails);
