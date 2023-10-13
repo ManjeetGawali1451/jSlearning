80912c0e57a9097ef64d2fdbf4a705cbcc47afc0
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-13 09:05:31 +0530

    taskCompleted
    taskCompleted

--- 09_objectCloningAssigA.js
+++ 09_objectCloningAssigA.js
@@ -20,14 +20,24 @@ console.log("=============================================");
 console.log("2--> Object Bank Location Created");
 console.log(bankLocation);
 console.log("=============================================");
-console.log("***** Clone the object  Bank sbi and Bank Location *****");
+console.log("3--> Clone the object  Bank sbi and Bank Location ");
 Object.assign(bankSbi,bankLocation)
 console.table(bankSbi)
 console.log("=============================================");
-console.log("Step 4");
 let rateOfInterest={
             homeLoanInterest:"8.1%",
             personalLoanInterest:"12.2%",
             duelInterest:"7%"        
 }
+console.log(" 4 --> Rate of interest literals");
 console.log(rateOfInterest);
+console.log("=============================================");
+console.log(" 5--> Merged Three object in sbi Details");
+let sbiDetails=Object.assign(bankSbi,bankLocation,rateOfInterest)
+console.table(sbiDetails);
+console.log("5-->Transversing The Merged object");
+for (const key in sbiDetails) {
+  console.log(`${key}: ${sbiDetails[key]}`);
+}
+
+
