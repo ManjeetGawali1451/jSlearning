ba49d1b4e813fedfa27a6e5ec905a825dd65f246
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-19 19:57:07 +0530

    step2 completed
    step2 completed

--- 23_arrowFunassignA.js
+++ 23_arrowFunassignA.js
@@ -1,7 +1,17 @@
 
-console.log("Assignment");
-console.log("step 1");
+console.log("---- Assignment ----");
+console.log("---- step 1 ------");
  let arrowFun =()=>{
          console.log(`Good Morning,Today is Thrusday`);           
  }
  arrowFun()
+console.log("*************************************");
+console.log("----- Step 2 ------");
+
+let arrowMul=(num1,num2,num3=1)=>{
+    let result=num1*num2*num3
+     console.log(`The multiplication of ${num1},${num2},${num3} --> ${result}`);
+     
+}
+arrowMul(5,5,2)
+arrowMul(10,4)
