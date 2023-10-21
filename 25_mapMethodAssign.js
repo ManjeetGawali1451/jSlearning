68ab6edfb540dfffe900d01ab25320be616768b9
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-21 14:28:03 +0530

    create 25_mapMethodAssign.js
    create 25_mapMethodAssign.js

---
+++ 25_mapMethodAssign.js
@@ -0,0 +1,10 @@
+console.log("----- Assignment ------");
+
+const arrayNumbers=[20,11,40,25,23,11,9,31,60,2,19]
+
+const arrayTransform= arrayNumbers.map((currentValue) =>{
+     return currentValue + 10
+
+})
+console.log(arrayTransform);
+
