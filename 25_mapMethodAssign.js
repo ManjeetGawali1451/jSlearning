c83aad7cf217eaf6225f207d18327bcda5bcfdcc
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-21 14:33:23 +0530

    step 2 completed
    step 2 completed

--- 25_mapMethodAssign.js
+++ 25_mapMethodAssign.js
@@ -8,3 +8,13 @@ const arrayTransform= arrayNumbers.map((currentValue) =>{
 })
 console.log(arrayTransform);
 
+console.log("----- Step 2 ----");
+
+const arrayCube = [20, 11, 40, 25, 23, 11, 9, 31, 60, 2, 19];
+
+const arrayTransform2= arrayCube.map((currentValue)=>{
+     return currentValue*currentValue*currentValue
+
+})
+   console.log(`Cubes of given array`);
+   console.log(arrayTransform2);
