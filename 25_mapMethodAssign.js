5aeffddf80cbcfa9f0d492f2e12046c91fa8cf8b
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-21 14:28:34 +0530

    step 1 completed
    step 1 completed

--- 25_mapMethodAssign.js
+++ 25_mapMethodAssign.js
@@ -1,5 +1,5 @@
 console.log("----- Assignment ------");
-
+console.log("------ step 1 ------");
 const arrayNumbers=[20,11,40,25,23,11,9,31,60,2,19]
 
 const arrayTransform= arrayNumbers.map((currentValue) =>{
