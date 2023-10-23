a4057abaaa741670c75394b41fd230af8ceb9307
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-23 15:17:41 +0530

    create 27_reduce.js
    create 27_reduce.js

--- 05_operator.js
+++ 05_operator.js
@@ -7,7 +7,7 @@ var num2 = 4;
 var result = num1 * num2;
 console.log(`Multiplication result is: ${result}`);
 
-var result = num2 ** 3;
+var result = num1 ** 3;
 console.log(`Exponential result is: ${result}`);
 
 var result = num1 / num2;
