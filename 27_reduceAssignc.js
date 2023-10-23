e1efa6df209eee6eef5f8bdc7f1a0cfe6a6617e4
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-23 16:00:47 +0530

    taskcompleted
    taskcompleted

--- 27_reduceAssignc.js
+++ 27_reduceAssignc.js
@@ -1,14 +1,24 @@
 console.log('--- Assignment ----');
-console.log('Reduce method');
+console.log('Step 1');
 const array_numbers=[20,11,40,25,37,49,9,90,60,2,19]
 const sum = array_numbers.reduce((runningTotal, value) => {
   return runningTotal + value;
 });
-console.log(`The addition of given array is --> ${sum}`);
+console.log(`The addition of given array by using reduce is  --> ${sum}`);
 console.log("--------------");
-console.log(`Addition in Traditional way`);
 let  sum2=0
 for (const element of array_numbers) {
       sum2= sum2 + element             
 }
-console.log(`The sum of given array is --> ${sum2}`);
+console.log(`The sum of given array in traditional way is  --> ${sum2}`);
+console.log(`-------------------------------`);
+console.log(` Step 2 `);
+const mulSum=array_numbers.filter((currentValue) =>{
+      return currentValue%5==0
+})
+console.log(`Numbers mulitiplied by 5 --> ${mulSum}`);
+
+const sum3=mulSum.reduce((runningTotal,value)=>{
+      return runningTotal+value
+})
+console.log(`The addition of multiply by 5 values is --> ${sum3}`);
