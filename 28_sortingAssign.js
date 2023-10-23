772661f21962dbea6b364e32f6685373f4a6c57b
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-23 20:58:33 +0530

    step1completted
    step1completted

---
+++ 28_sortingAssign.js
@@ -0,0 +1,6 @@
+console.log(`----- Assignment -----`);
+console.log(`----- Step 1 -------`);
+const array=[113,45,56,11,32,45,109,799,56,45]
+console.log(`Given array --> ${array}`);
+let reverseArray=array.reverse()
+console.log(`Reversed Array --> ${reverseArray}`);
