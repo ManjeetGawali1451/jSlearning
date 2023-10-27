e5117c0ff7a69401f19401042946192c82e46a17
ManjeetGawali1451 <manjeetgavali2001@gmail.com>
2023-10-27 19:00:38 +0530

    taskcompletd
    taskcompletd

---
+++ 31_main.js
@@ -0,0 +1,10 @@
+
+const heading1=document.querySelector(`#h1`)
+heading1.style.color="orange"
+
+const listSelect=document.querySelector(`#list`)
+const agileElement=document.querySelector(`#Agile`)
+listSelect.removeChild(agileElement)
+
+const heading2=document.querySelector(`#h2`)
+heading2.style.color="red"
